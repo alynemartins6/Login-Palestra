71e29f8a8d18289d14759830503131cbb97cccc4
Leonardo Meireles <leomeireles55@hotmail.com>
2024-05-25 17:49:18 -0300

    chore: Refactor login form validation and API request

--- script-cadastro.js
+++ script-cadastro.js
@@ -1,66 +1,71 @@
-const inputNome = document.getElementById('inputNome');
-const inputSobrenome =document.getElementById('inputSobrenome');
-const inputMatricula =document.getElementById('inputMatricula');
-const inputCpf =document.getElementById('inputCpf');
-const form = document.querySelector('form');
-const buttonCadastrar = document.getElementById('submit-cadastrar');
+const inputNome = document.getElementById("inputNome");
+const inputSobrenome = document.getElementById("inputSobrenome");
+const inputMatricula = document.getElementById("inputMatricula");
+const inputCpf = document.getElementById("inputCpf");
+const form = document.querySelector("form");
+const buttonCadastrar = document.getElementById("submit-cadastrar");
 
-buttonCadastrar.addEventListener('click', (event) => {
-    event.preventDefault(); 
-  
-    const isValid = validateForm(); 
-  
-    if (isValid) {
-      let formulario = new Object();
-      formulario.nome = inputNome.value;
-      formulario.sobreNome = inputSobrenome.value;
-      formulario.email = inputEmail.value;
-      formulario.senha = inputSenha.value;
-      formulario.matricula = inputMatricula.value;
-      formulario.cpf = inputCpf.value;
-      console.log('Formulário enviado com sucesso!'+ JSON.stringify(formulario));
+buttonCadastrar.addEventListener("click", async (event) => {
+	event.preventDefault();
 
-      fetch("http://localhost:8080/cadastro",{"method":"POST","formulario":JSON.stringify(formulario)})
-      .then(function(resp){
-        if (!resp.ok) throw new Error("Falha ao cadastrar usuário");
-        return resp.json(); })
-      .then( jason => alert("Cadastro realizado com sucesso"))
-      .catch(err => alert("Falha ao realizar cadastro" + err));
+	const isValid = validateForm();
 
-    };
-  });
+	if (isValid) {
+		let formulario = new Object();
+		formulario.firstName = inputNome.value;
+		formulario.lastName = inputSobrenome.value;
+		formulario.cpf = inputCpf.value;
+		formulario.registerID = inputMatricula.value;
+		formulario.email = inputEmail.value;
+		formulario.password = inputSenha.value;
+		console.log("Formulário enviado com sucesso!" + JSON.stringify({ ...formulario }));
+
+		await fetch("http://localhost:3000/user/signup", {
+			method: "POST",
+			headers: {
+				Accept: "*/*",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formulario),
+		})
+			.then(function (resp) {
+				if (!resp.ok) throw new Error("Falha ao cadastrar usuário");
+				return resp.json();
+			})
+			.then((json) => alert("Cadastro realizado com sucesso"))
+			.catch((err) => alert("Falha ao realizar cadastro: " + err));
+	}
+});
 
 function validateForm() {
-let isValid = true;
+	let isValid = true;
 
-    if (inputNome.value === '') {
-        alert('O campo "Primeiro Nome" é obrigatório.');
-        isValid = false;
-    }
-    if (inputSobrenome.value === '') {
-        alert('O campo "Sobrenome" é obrigatório.');
-        isValid = false;
-    }
-    if (inputEmail.value === '') {
-        alert('O campo "Email" é obrigatório.');
-        isValid = false;
-    }
-    if (inputSenha.value === '') {
-        alert('O campo "Senha" é obrigatório.');
-        isValid = false;
-    } else if (inputSenha.value.length < 8) {
-        alert('A senha deve ter pelo menos 8 caracteres.');
-        isValid = false;
-    }
-    if (inputMatricula.value === '') {
-        alert('O campo "Matrícula" é obrigatório.');
-        isValid = false;
-    }
-    if (inputCpf.value === '') {
-        alert('O campo "CPF" é obrigatório.');
-        isValid = false;
-    }
-    return isValid; 
-
+	if (inputNome.value === "") {
+		alert('O campo "Primeiro Nome" é obrigatório.');
+		isValid = false;
+	}
+	if (inputSobrenome.value === "") {
+		alert('O campo "Sobrenome" é obrigatório.');
+		isValid = false;
+	}
+	if (inputEmail.value === "") {
+		alert('O campo "Email" é obrigatório.');
+		isValid = false;
+	}
+	if (inputSenha.value === "") {
+		alert('O campo "Senha" é obrigatório.');
+		isValid = false;
+	} else if (inputSenha.value.length < 8) {
+		alert("A senha deve ter pelo menos 8 caracteres.");
+		isValid = false;
+	}
+	if (inputMatricula.value === "") {
+		alert('O campo "Matrícula" é obrigatório.');
+		isValid = false;
+	}
+	if (inputCpf.value === "") {
+		alert('O campo "CPF" é obrigatório.');
+		isValid = false;
+	}
+	return isValid;
 }
-  
--- script-index.js
+++ script-index.js
@@ -1,49 +1,50 @@
 /// Tela de login ////
-  const inputEmail = document.getElementById('inputEmail');
-  const inputSenha = document.getElementById('inputSenha');
-  const buttonEnviar = document.getElementById('submit-enviar');
-  const buttonCadastrar = document.getElementById('submit-cadastrar');
+const inputEmail = document.getElementById("inputEmail");
+const inputSenha = document.getElementById("inputSenha");
+const buttonEnviar = document.getElementById("submit-enviar");
+const buttonCadastrar = document.getElementById("submit-cadastrar");
 
-  buttonEnviar.addEventListener('click', (event) => {
-    event.preventDefault();
+buttonEnviar.addEventListener("click", async (event) => {
+	event.preventDefault();
 
-    let isValid = true;
+	let isValid = true;
 
-    if (inputEmail.value === '') {
-      alert('Por favor, preencha o campo de email.');
-      isValid = false;
-    }
-   
-    if (inputSenha.value === '' || inputSenha.value.length < 8) {
-      alert('Por favor, preencha a senha com pelo menos 8 caracteres.');
-      isValid = false;
-    }
+	if (inputEmail.value === "") {
+		alert("Por favor, preencha o campo de email.");
+		isValid = false;
+	}
 
-    else if (isValid) {
-      let body = new Object();
-      body.usuario = inputEmail.value;
-      body.senha = inputSenha.value;
+	if (inputSenha.value === "" || inputSenha.value.length < 8) {
+		alert("Por favor, preencha a senha com pelo menos 8 caracteres.");
+		isValid = false;
+	} else if (isValid) {
+		let body = new Object();
+		body.email = inputEmail.value;
+		body.pass = inputSenha.value;
+		x;
+		await fetch("http://localhost:3000/auth/login", {
+			method: "POST",
+			headers: {
+				Accept: "*/*",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
+		})
+			.then(async function (resp) {
+				if (!resp.ok) throw new Error("Falha ao cadastrar usuário");
+				const data = await resp.json();
+				localStorage.setItem("token", data.token);
+				alert("Login realizado com sucesso");
+				console.log(data);
+				return data;
+			})
+			.then((json) => alert("Login realizado com sucesso"))
+			.catch((err) => alert("Falha ao realizar login: " + err));
+		///.finally(() => document.location.href="/pagina-principal.html" )
+	}
+});
 
-      console.log('Formulário enviado com sucesso!' + JSON.stringify(body));
-
-      fetch("http://localhost:8080/login",{"method":"POST","body":JSON.stringify(body)})
-      .then(function(resp){
-        if (resp.status != 200) throw new Error("Login invalido");
-        return resp.json();
-      })
-      .then( json => document.location.href = "/pagina-principal.html")
-      .catch(err => alert("Falha ao realizar login" + err))
-      ///.finally(() => document.location.href="/pagina-principal.html" )
-    }
-  });
-
-  buttonCadastrar.addEventListener('click',(event) =>{
-  event.preventDefault();
-  document.location.href="/cadastro.html"
-
-  });
-
-
-
-
-
+buttonCadastrar.addEventListener("click", (event) => {
+	event.preventDefault();
+	document.location.href = "/cadastro.html";
+});
